0315b29bdb8f3ba3f4688e7212514f22da8a779e
Rafael Bahiense <rafaelbahiense@id.uff.br>
2023-09-16 20:42:18 -0300

    project setup

---
+++ services/api/serverless.ts
@@ -0,0 +1,55 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'api',
+  frameworkVersion: '3',
+  plugins: [
+    'serverless-esbuild',
+    'serverless-domain-manager'
+  ],
+  useDotenv: true,
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs18.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: { hello },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node18',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    domain: {
+      prod: "${env:DOMAIN}",
+    },
+    subdomain: {
+      prod: "${env:SUBDOMAIN}",
+    },
+    customDomain: {
+      domainName: "${self:custom.subdomain.${sls:stage}}",
+      basePath: "v1",
+      stage: "${sls:stage}",
+      certificateName: "${self:custom.domain.${sls:stage}}",
+      createRoute53Record: true,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
